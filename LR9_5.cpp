a4397edde835088facfe73356910970948af952f
EnverAbiltarov <122446374+EnverAbiltarov@users.noreply.github.com>
2023-01-11 13:51:47 +0300

    First commit

---
+++ LR9_5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int hour = -1, min = -1, sec = -1;
+	while ((hour < 0) or (hour > 23)) {
+		cout << "Type hours : ";
+		cin >> hour;
+	}
+	while ((min < 0) or (min > 59)) {
+		cout << "Type minutes : ";
+		cin >> min;
+	}
+	while ((sec < 0) or (sec > 59)) {
+		cout << "Type seconds : ";
+		cin >> sec;
+	}
+	cout << "Seconds: " << hour * 3600 + min * 60 + sec;
+	cin.get();
+	cin.get();
+	return 0;
+}
