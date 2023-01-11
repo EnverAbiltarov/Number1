d52c25ffdda90823e32a58f9b2b7e14878872afa
EnverAbiltarov <122446374+EnverAbiltarov@users.noreply.github.com>
2023-01-11 13:58:36 +0300

    second commit

---
+++ LR9_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int s = 0, p = 1, val = 1;
+	while (val != 0) {
+		cout << "Enter the value: ";
+		cin >> val;
+		if ((val > 10) and (val < 25)) {
+			s += val;
+		}
+		if (val > 15) {
+			p *= val;
+		}
+	}
+	if (s < p) {
+		cout << "Sum: " << s;
+	}
+	else {
+		cout << "Multiplying: " << p;
+	}
+	cin.get();
+	cin.get();
+	return 0;
+}
